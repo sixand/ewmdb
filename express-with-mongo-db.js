40e5d1f60f03f3821a32b5598cc89d3b133b25e1
sixand <sixand@gmail.com>
2018-05-03 17:16:11 +0800

    Create express-with-mongo-db.js

---
+++ express-with-mongo-db.js
@@ -0,0 +1,38 @@
+'use latest';
+import bodyParser from 'body-parser';
+import express from 'express';
+import Webtask from 'webtask-tools';
+import { MongoClient } from 'mongodb';
+import { ObjectID } from 'mongodb';
+
+const collection = 'my-collection';
+const server = express();
+
+
+server.use(bodyParser.json());
+server.get('/:_id', (req, res, next) => {
+  const { MONGO_URL } = req.webtaskContext.secrets;
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    const { _id } = req.params ;
+    if (err) return next(err);
+    db.collection(collection).findOne({ _id: new ObjectID(_id) }, (err, result) => {
+      db.close();
+      if (err) return next(err);
+      res.status(200).send(result);
+    });
+  });
+});
+server.post('/', (req, res, next) => {
+  const { MONGO_URL } = req.webtaskContext.secrets;
+  // Do data sanitation here.
+  const model = req.body;
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) return next(err);
+    db.collection(collection).insertOne(model, (err, result) => {
+      db.close();
+      if (err) return next(err);
+      res.status(201).send(result);
+    });
+  });
+});
+module.exports = Webtask.fromExpress(server);
